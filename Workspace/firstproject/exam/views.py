5aac53ceb0a12cac13a297f0c79b030bd57c7b07
Sachin Dhiman <sachindhiman912@gmail.com>
2024-02-25 21:53:06 +0530

    Templates

--- Workspace/firstproject/exam/views.py
+++ Workspace/firstproject/exam/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 def testpaper(request):
-    s="<h1>This is a test paper</h1>"
-    return HttpResponse(s)
+    que="who developed Python?"
+    a="Dennis Richie"
+    b="Guido van Rousom"
+    c="Ken Thomsan"
+    d="Bajarne Stroustrup"
+    context={
+        'que':que,
+        'options':[a,b,c,d]
+
+    }
+    template=loader.get_template('testpaper.html')
+    res=template.render(context,request)
+    return HttpResponse(res)
 
 def result(request):
     s="<h1>Result</h1>"
--- Workspace/firstproject/testapp/views.py
+++ Workspace/firstproject/testapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 def greetings(request):
     s="<h1>Hello and Welcome to first view</h1>"
     return HttpResponse(s)
 
 def about(request):
-    s="<h1>About Page</h1>"
-    return HttpResponse(s)
+    template=loader.get_template('about.html')
+    res=template.render()
+    return HttpResponse(res)
 
 def contact(request):
     s="<h1>Contact Page</h1>"
